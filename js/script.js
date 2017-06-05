0fda571cc002a7f06a748c7241ce8679bb2ec210
Aizhan <aiyerimbetova@gmail.com>
2017-06-05 11:15:38 +0600

    responsive carousel

--- js/script.js
+++ js/script.js
@@ -1,23 +1,7 @@
-$('#myCarousel').carousel({
-  interval: 4000
-})
-
-$('.carousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  
-  for (var i=0;i<2;i++) {
-    next=next.next();
-    if (!next.length) {
-        next = $(this).siblings(':first');
-    }
-    
-    next.children(':first-child').clone().appendTo($(this));
-  }
+$('.carousel.slide').carousel({
+  interval: 3000
 });
+
 
 
 $(document).ready(function(){
